7f6506c7523e1ab8ce7a88c956831caa2fe8ebb1
John Bynum <iexclusiveproductions@gmail.com>
2022-11-02 15:10:58 -0400

    fixing backend to work with MongoDB

--- server.js
+++ server.js
@@ -2,5 +2,32 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 7000;
+const port = 7001;
 
+// Connection string to local instance of MongoDB including database
+const connectionStringURI = 'mongodb://127.0.0.1:27017/socialnetworkDB',
+
+// Declare a variable to hold the connection 
+let db;
+
+mongodb.connect(
+    connectionStringURI,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err, client) => {
+        db = client.db();
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`)
+        })
+    }
+
+)
+app.use(express.json());
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI ||  {
+    
+});
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
