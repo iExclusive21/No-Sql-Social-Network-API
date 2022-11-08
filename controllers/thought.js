955c0ff315ff4c989da68b9a3ace15ae9e8f5c4b
John Bynum <iexclusiveproductions@gmail.com>
2022-11-08 18:21:50 -0500

    working on thoughts page

--- controllers/thought.js
+++ controllers/thought.js
@@ -10,7 +10,35 @@ const thoughtsController = {
                 console.log(err);
                 res.status(500).json(err);
             });
-    };
+    },
 
+    getThoughtsByID(req, res){
+        thoughts.findOne({ _id: req.params.thoughtsID})
+        .then((socialNetworkDb) => {
+            res.json(socialNetworkDb);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+        
+    }, 
+
+    createNewThoughts(req, res){
+        thoughts.create(req.body)
+        .then((userDataDB)=>{
+            res.json({message:'Thought created'});
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    editThoughts(req, res) {
+        thoughts.findOneAndUpdate({                 })
+    }
     
+
+
 }
