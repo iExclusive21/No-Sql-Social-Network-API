f5cb35cd7740fa66ef49c1d58ec911ea8251eccf
John Bynum <iexclusiveproductions@gmail.com>
2022-11-12 11:13:20 -0500

    thoughts completed

--- controllers/thought.js
+++ controllers/thought.js
@@ -1,4 +1,5 @@
 const { thoughts, users } = require('../models');
+const User = require('../models/users');
 
 const thoughtsController = {
     getThoughts(req, res) {
@@ -34,14 +35,73 @@ const thoughtsController = {
             res.status(500).json(err);
         });
     },
+    
+    deleteNewThoughts(req, res) {
+        thoughts.findOneAndRemove({_id: req. params.thoughtsID})
+            .then((userDataDB) => {
+                
+                return User.findOneAndUpdate(
+                    { thoughts: req.params.thoughtID },
+                    { $pull: { thoughts: req.params.thoughtId } },
+                    { new: true }
+                );
+
+            })
+            .then((userDataDB) => {
+                res.json({message: "Thought deleted."});
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 
     editThoughts(req, res) {
         thoughts.findOneAndUpdate({_id: req.params.thoughtsID},{$set: req.body}, { runValidators: true, new: true})
         .then((userDataDB) => {
             res.json(userDataDB);
         })
-    }
-    
+        .catch((err) =>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
 
+    removeReaction(req, res) {
+      Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      )
+        .then((userDataDB) => {
+         
+          res.json(userDataDB);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
 
-}
+    addReaction({params, body}, res){
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$push: {reactions: body}},
+            { new: true, runValidators: true }
+        )
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(userDataDB => {
+            if (!userDataDB) {
+                res.status(404).json({ message: 'Incorrect reaction!' });
+                return;
+            }
+            res.json(userDataDB);
+        }).catch(err=> {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+};    
+
+module.exports = thoughtsController;
