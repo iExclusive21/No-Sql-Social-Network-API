97d2c21ce04b6fc01e59e228930c1d22adb2910c
John Bynum <iexclusiveproductions@gmail.com>
2022-11-12 18:11:14 -0500

    changed my serverjs

--- server.js
+++ server.js
@@ -6,59 +6,17 @@ const routes = require('./routes');
 const app = express();
 const port = 7001;
 
-// Connection string to local instance of MongoDB including database
-const connectionStringURI = 'mongodb://127.0.0.1:27017/socialnetworkDB';
-
-// Declare a variable to hold the connection 
-var db;
-
-mongodb.connect(
-    connectionStringURI,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    (err, client) => {
-        db = client.db();
-        app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`)
-        })
-    }
-
-)
-app.use(routes)
 app.use(express.json());
-app.post('/create', (req, res) => {
-    db.collection('socialNetwork').insertOne(
-      { title: req.body.title, author: req.body.author },
-      (err, results) => {
-        if (err) throw err;
-        res.json(results);
-      }
-    );
-  });
-
-  app.get('/read', (req, res) => {
-    db.collection('socialNetwork')
-      .find()
-      .toArray((err, results) => {
-        if (err) throw err;
-        res.send(results);
-      });
-  });
-
-  app.delete('/delete', (req, res) => {
-    // Use deleteOne() to delete one object
-    db.collection('socialNetwork').deleteOne(
-      // This is the filter. We delete only the document that matches the _id provided in the request body,
-      { _id: ObjectId(req.body.id) },
-      (err) => {
-        if (err) throw err;
-        res.send("Document deleted");
-      }
-    );
-  });
-
-
-// app.use(require('./routes'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
